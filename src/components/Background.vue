47f4fcce57db243ef6bf599496292bf221dba81a
Gray <35620337+Gray97@users.noreply.github.com>
2026-05-17 15:45:30 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -43,9 +43,9 @@ const changeBg = (type) => {
   } else if (type == 1) {
     bgUrl.value = "https://api.dujin.org/bing/1920.php";
   } else if (type == 2) {
-    bgUrl.value = "https://api.r10086.com/PPT/PPT.php?PPT=火影忍者,赛马娘,龙珠";
+    bgUrl.value = "https://t.alcy.cc/fj";
   } else if (type == 3) {
-    bgUrl.value = "https://t.alcy.cc/acg";
+    bgUrl.value = "http://www.98qy.com/sjbz/api.php";
   }
 };
 

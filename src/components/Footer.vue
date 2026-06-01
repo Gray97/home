f2eba4eb996a432919119d80fc6b645bc595f2bb
Gray <35620337+Gray97@users.noreply.github.com>
2026-06-01 15:21:21 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,7 +11,7 @@
             -
           </span>
           {{ fullYear }}
-          <a :href="siteUrl" style="color: pink;" >{{ siteAuthor }}</a>
+          <a :href="siteUrl" style="color: blue;" >{{ siteAuthor }}</a>
         </span>
         <!-- 以下信息请不要修改哦 -->
         <span class="hidden">

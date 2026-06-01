a15fe1320adb3b93b07d48591b69cadc892413ec
Gray <35620337+Gray97@users.noreply.github.com>
2026-06-01 16:16:14 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -20,7 +20,7 @@
         <!-- 站点备案 -->
         <span>
           &amp;
-          <a v-if="siteIcp" href="https://icp.gov.moe/join.php?id=20260567" target="_blank" style="color: pink;">
+          <a v-if="siteIcp" href="https://icp.gov.moe/?keyword=20260567" target="_blank" style="color: pink;">
             {{ siteIcp }}
           </a>
         </span>

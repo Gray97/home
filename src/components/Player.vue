2fcf47d86a5d2f226916d5fc4dd4f2554569d0ca
Gray <35620337+Gray97@users.noreply.github.com>
2026-06-04 15:43:05 +0800

    Update Player.vue

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -7,6 +7,8 @@
     :theme="theme"
     :autoSwitch="false"
     :loop="store.playerLoop"
+    "store.playerOrder"
+     <!-- :order="store.playerOrder"为顺序播放；:order="'random'"为随机播放-->
     :order="'random'"
     :volume="volume"
     :showLrc="true"
